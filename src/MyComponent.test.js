84b2dd885e40d299b63ea596ffc917faeb83dcc0
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-19 15:53:49 -0600

    Learn to test by text

--- src/MyComponent.test.js
+++ src/MyComponent.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { render, unmountComponentAtNode } from "react-dom";
 import { fireEvent, screen, act, render } from '@testing-library/react';
 import MyComp from './MyComponent';
 
-let container = null;
 
 function click(txt) {
     const regTxt = new RegExp(txt, 'i')
@@ -13,39 +11,21 @@ function click(txt) {
 const testCounterFn = jest.fn();
 
 
-// beforeEach(() => {
-//     container = document.createElement("div");
-//     document.body.appendChild(container);
-// });
-
-// afterEach(() => {
-//     // cleanup on exiting
-//     unmountComponentAtNode(container);
-//     container.remove();
-//     container = null;
-//   });
-
 test('test my component plumbing', () => {
     // console.log('Hey we\'re testing')
-    act(() => {
-        render(<MyComp counterFunction={testCounterFn} />);
-        click('component counter');
-        click('component counter');
-        click('other count');
-        click('other count');
-        click('other count');
-        click('other count');
-    });
+   render(<MyComp counterFunction={testCounterFn} countme={10} />);
+    click('The component counter is');
+    click('The component counter is');
+    click('The component counter is');
+    click('The component counter is');
+    click('The other count is');
+    click('The other count is');
+    click('The other count is');
 
-    // const conta = container.querySelector('H2');
-    // console.log('container:', container.textContent);
-
+    // console.log('text:', screen.getByText(/The component counter/i).innerHTML);
+    expect(testCounterFn.mock.calls.length).toBe(3);
+    expect(screen.getByText(/The component counter/i).innerHTML).
+        toBe('The component counter is at 104');
     screen.debug();
-    console.log('length:', testCounterFn.mock.calls.length); 
-    // console.log('calls:', testCounterFn.mock); 
-    //   console.log('screen:', screen.textContent);
-    // const el = screen.getByText(/check out this cool/i);
-
-    // expect(container.textContent).toContain('Check out this cool component!');
-    // expect(container.textContent).toContain('The component counter is');
 });
+
